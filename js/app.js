a8fdda0cf4ca6ea9929e927468a4dddce39ac93e
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-07-20 20:04:24 +0800

    I've finished this project fixing any bug I've saw.

--- js/app.js
+++ js/app.js
@@ -4,38 +4,74 @@
 // preciso entregar uma selacao de quem tirou quem no jogo do amigo secreto;
 //  - No final tenho que dar a opcao de a pessoa poder limpar 
 // tudo caso ela queira para iniciar uma nova lista.
-let listaAmigos = document.getElementById('lista-amigos');
+// * primeiro problema que vou resolver e que quando colocamos o primeiro nome na lista,
+// os seguintes podem ficar vazios, entao temos que mandar um alerta que vao pode ser 
+// adicionado nada se o campo estiver vazio.
+let amigos = [];
 
 
 function adicionar() {
     let nomeAmigo = document.getElementById('nome-amigo').value;
+    let listaAmigos = document.getElementById('lista-amigos');
 
-    if (listaAmigos.textContent == '') {
+
+    if (nomeAmigo == '') {
+        alert('Nao pode ser adicionado um valor vazio.')
+    } else if (amigos.includes(nomeAmigo)) {
+        alert('Este amigo ja esta na lista de amigos.')
+    } else if (listaAmigos.textContent == '') {
         listaAmigos.textContent = nomeAmigo;
+        amigos.push(nomeAmigo);
     } else {
         listaAmigos.textContent += ', ' + nomeAmigo;
+        amigos.push(nomeAmigo);
     }
 
+    
     document.getElementById('nome-amigo').value = '';
 
-    console.log(listaAmigos.textContent.split(', '))
+    console.log(amigos)
+
 }
 
+function embaralharLista(array) {
+  let currentIndex = array.length, randomIndex;
 
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
 
-function getRandomItemList(array) {
-    let randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
-
 
 function sortear() {
-    let nomeSorteado = document.getElementById('lista-sorteio');
-    let primeiraListaSorteio = [];
-    let segundaListaSorteio = [];
-    for (let i = 0; i < listaAmigos; i++) {
-        primeiraListaSorteio.push(getRandomItemList(listaAmigos));
-        console.log(primeiraListaSorteio)
+    let listaAmigosSorteados = embaralharLista(amigos);
+    let listaSorteio = document.getElementById('lista-sorteio');
+
+    if (amigos.length == 1) {
+        alert('Nao podemos sortear apenas uma pessoa.')
+    } else {
+            for (let i = 0; i < amigos.length; i++) {
+
+        if (i == amigos.length - 1) {
+        listaSorteio.innerHTML += listaAmigosSorteados[i] + ' --> ' + listaAmigosSorteados[0] + '<br>';
+        } else {
+        listaSorteio.innerHTML += listaAmigosSorteados[i] + ' --> ' + listaAmigosSorteados[i + 1] + '<br>';
+        }
     }
-    console.log(primeiraListaSorteio)
+    }
+}
+
+function reiniciar() {
+    document.getElementById('nome-amigo').value = '';
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+
 }
